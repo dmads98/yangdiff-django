0d462692c73e3caf1e23b8febf81899903d7bae0
Dhanush Madabusi <dm322@duke.edu>
2019-07-23 16:35:01 -0700

    created views and templates dir

--- src/yangdiff_django/urls.py
+++ src/yangdiff_django/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import example_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', example_page)
 ]
