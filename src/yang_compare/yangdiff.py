f07d069085430b19c7543bafa73225f9a9d742df
Dhanush Madabusi <dm322@duke.edu>
2019-08-13 17:48:42 -0700

    error handling on script

--- src/yang_compare/yangdiff.py
+++ src/yang_compare/yangdiff.py
@@ -6,29 +6,30 @@ def fileCompare(oldvers, oldfile, newvers, newfile):
 	header = "false"
 	create_yang_old_directory()
 	create_yang_new_directory()
-	create_output_directory()
 	getAndOrModifyFiles(oldvers, oldfile, True)
 	getAndOrModifyFiles(newvers, newfile, False)
-	if (check_for_valid_files(oldfile[:-5] + "-" + oldvers, "yang_old") and check_for_valid_files(newfile[:-5], "yang_new")):
-		subpr = subprocess.run([
-			"yangdiff-pro",
-			"--old=yang_old/" + oldfile[:-5] + "-" + oldvers + ".yang",
-			"--new=yang_new/" + newfile,
-			"--modpath=" + "yang_old:yang_new",
-			"--difftype=" + diff_type,
-			"--header=" + header], capture_output=True, text=True)
-		output = subpr.stdout.strip('\n')
-		if output.startswith("Error"):
-			print("Error(s) occurred - Comparison unsuccessful")
-			error = output[7:output.find("yangdiff")]
-			error = error[:1].upper() + error[1:]
-			error = error.strip('\n')
-			print(error)
-		else:
-			file = open("diff_output/diff.txt", "w+")
-			file.write(output)
-			file.close()
-			print(output)
+	result = check_for_valid_files(oldfile[:-5] + "-" + oldvers, "yang_old")
+	if result['errors']:
+		return result
+	result = check_for_valid_files(newfile[:-5], "yang_new")
+	if result['errors']:
+		return result
+	subpr = subprocess.run([
+		"yangdiff-pro",
+		"--old=yang_old/" + oldfile[:-5] + "-" + oldvers + ".yang",
+		"--new=yang_new/" + newfile,
+		"--modpath=" + "yang_old:yang_new",
+		"--difftype=" + diff_type,
+		"--header=" + header], capture_output=True, text=True)
+	output = subpr.stdout.strip('\n')
+	errors = []
+	if output.startswith("Error"):
+		print("Error(s) occurred - Comparison unsuccessful")
+		error = output[7:output.find("yangdiff")]
+		error = error[:1].upper() + error[1:]
+		error = error.strip('\n')
+		errors.append(error)
+	return {"output": output, "errors": errors}
 
 def create_yang_old_directory():
 	subpr = subprocess.run([
@@ -39,6 +40,17 @@ def create_yang_new_directory():
 	subpr = subprocess.run([
 		"mkdir",
 		"yang_new"])
+
+def delete_yang_directories():
+	subprocess.run([
+		"rm",
+		"-r",
+		"yang_old"])
+	subprocess.run([
+		"rm",
+		"-r",
+		"yang_new"])
+
 
 def getAndOrModifyFiles(vers, file, old):
 	file_list = [file[:-5]]
@@ -103,31 +115,27 @@ def check_for_valid_files(file, dir_name):
 		"-p",
 		dir_name], capture_output=True, text=True)
 	if subpr.stderr != "":
-		print("Error(s) occurred - File set invalid")
+		message = "Error(s) occurred - File set invalid"
 		index = subpr.stderr.find("]")
 		if index != -1:
-			print((subpr.stderr[index + 2:]).strip('\n'))
-			return False
+			return {"errors": [message, (subpr.stderr[index + 2:]).strip('\n')]}
 		errors = subpr.stderr.splitlines()
 		for line in errors:
 			line = line[line.find("error: ") + 7:]
 			line = line[:1].upper() + line[1:]
-			if not line.startswith("Module"):
-				print("    ", line)
-			else:
-				print(line)
-		return False
+		errors[:0] = message
+		return {"errors": errors}
 	output = subpr.stdout.strip('\n')
 	if output == "":
-		print("Please select a primary module to compare. You have selected a submodule or types file." +
-			"\nA node tree cannot be constructed from this file.")
-		return False
-	return True
+		return {"errors":  ["Please select a primary module to compare. You have selected a submodule or types file." +
+			"\nA node tree cannot be constructed from this file."]}
+	return {"errors": []}
+
+def main():
+	delete_yang_directories()
+	result = fileCompare("633", "Cisco-IOS-XR-cdp-oper.yang", "652", "Cisco-IOS-XR-cdp-oper.yang")
+	print(result)
 
 if __name__ == "__main__":
-	oldvers = "633"
-	oldfile = "Cisco-IOS-XR-cdp-oper.yang"
-	newvers = "652"
-	newfile = "Cisco-IOS-XR-cdp-oper.yang"
-	fileCompare(oldvers, oldfile, newvers, newfile)
+	main()
 
